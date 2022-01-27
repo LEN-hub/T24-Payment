c6e35733ecfbda7ca52d6d71a59c05581eeae473
yk950610 <617558302@qq.com>
2022-01-27 15:15:35 +0800

    test code commit by Kevin

--- src/test/java/com/hsbc/cmb/hk/dbb/WebRunner.java
+++ src/test/java/com/hsbc/cmb/hk/dbb/WebRunner.java
@@ -4,14 +4,12 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-/**
- * Created by Tom on 10/07/2019.
- */
+
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features",
         plugin = {"pretty","html:target/cucumber","json:target/json/cucumber.json"},
-        tags = "@ClientProfile")
+        tags = "@ext")
 public class WebRunner {
 
 }
