4f5a859d9c2016e8c79e404fe32c42dddb0210cd
leilv <3111969204@qq.com>
2024-09-11 14:14:04 +0800

    testgbp

--- src/test/java/com/glbank/com/sg/bdd/steps/T24/Payment/t24_Payments_step.java
+++ src/test/java/com/glbank/com/sg/bdd/steps/T24/Payment/t24_Payments_step.java
@@ -4742,14 +4742,14 @@ public class t24_Payments_step extends ScenarioSteps {
             t24_payments_page.CAHKD.click();
             bddUtil.sleep(5);
         } else if (SettlementAcc=="AUD") {
-            t24_payments_page.CAEUR.click();
+            t24_payments_page.CAAUD.click();
             bddUtil.sleep(5);
         } else if (SettlementAcc=="AED") {
             t24_payments_page.CAAED.click();
             bddUtil.sleep(5);
         }
         else if (SettlementAcc=="GBP") {
-            t24_payments_page.CAGBP.click();
+            t24_payments_page.CAEUR.click();
             bddUtil.sleep(5);
         }
         else if (SettlementAcc=="JPY") {
@@ -4757,7 +4757,7 @@ public class t24_Payments_step extends ScenarioSteps {
             bddUtil.sleep(5);
         }
         else {
-            t24_payments_page.CAAUD.click();
+            t24_payments_page.CAGBP.click();
             bddUtil.sleep(5);
         }
         bddUtil.switchToNewWindow();
